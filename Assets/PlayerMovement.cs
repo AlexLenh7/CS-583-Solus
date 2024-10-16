3bc46a2f2bb1654a5cab76b537e9401978fbde23
AlexLenh7 <alexlenh666@gmail.com>
2024-10-15 21:46:14 -0700

    Basic movement + import assets

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    private Vector2 moveDirection;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        inputs();
     }
+
+    void FixedUpdate() 
+    {
+        //physics
+        Move();
+    }
+
+    void inputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
 }
