cbfbd79dbea47bbd139807c38a541351d0571d03
AlexLenh7 <alexlenh666@gmail.com>
2024-10-27 01:34:01 -0700

    refined char movement / attack
    make basic changes to movement and attack animation handling + begun tile map

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,34 +4,80 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed;
+    public float moveSpeed = 1f;
     public Rigidbody2D rb;
     private Vector2 moveDirection;
+    public Animator animator;
+    private Vector2 lastMoveDirection;
+    public bool canMove = true;
+    public float attackStagger = 0.2f;
     // Start is called before the first frame update
 
     // Update is called once per frame
     void Update()
     {
-        inputs();
+        
+        if (canMove)
+        {
+            float moveX = Input.GetAxisRaw("Horizontal");
+            float moveY = Input.GetAxisRaw("Vertical");
+            moveDirection = new Vector2(moveX, moveY).normalized;
+
+            animator.SetFloat("Horizontal", moveDirection.x);
+            animator.SetFloat("Vertical", moveDirection.y);
+            animator.SetFloat("Speed", moveDirection.sqrMagnitude);
+
+            if (moveDirection.sqrMagnitude > 0.01f)
+            {
+                lastMoveDirection = moveDirection;
+                animator.SetFloat("LastHorizontal", moveDirection.x);
+                animator.SetFloat("LastVertical", moveDirection.y);
+            }
+        }
+        else
+        {
+            moveDirection = Vector2.zero;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayAttackAnimation();
+        }
+
     }
 
     void FixedUpdate() 
     {
-        //physics
-        Move();
+        // Only move the player if not attacking (you could allow movement while attacking if needed)
+        if (canMove)
+        {
+            rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        } 
+        else
+        {
+            rb.velocity = Vector2.zero; // Stop movement while attacking
+        }
     }
 
-    void inputs()
+    void PlayAttackAnimation()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
-
-        moveDirection = new Vector2(moveX, moveY).normalized;
+        canMove = false;
+        animator.SetTrigger("Attack");
+        // animator.SetBool("IsAttacking", true);
+        // rb.velocity = lastMoveDirection * attackStagger;
+        animator.SetFloat("LastHorizontal", lastMoveDirection.x);
+        animator.SetFloat("LastVertical", lastMoveDirection.y);
     }
 
-    void Move()
+    // void StopAttacking()
+    // {
+    //     animator.SetBool("IsAttacking", false);
+    // }
+
+    void FinishAttack()
     {
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        canMove = true;
+        // rb.velocity = Vector2.zero;
     }
 
 }
